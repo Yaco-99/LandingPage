b6c52cc12dbd8ca936b08c88474320dc915fd721
Yaco-99 <coopman.yannick@gmail.com>
2020-10-12 15:13:29 +0200

    landingPage v2.1 bootstrap optimisation

--- assets/js/requestScript.js
+++ assets/js/requestScript.js
@@ -1,55 +1,56 @@
-const openModalButton = document.getElementById("open-modal-button");
-const closeModalButtons = document.querySelectorAll("[data-close-button]");
-const overlay = document.getElementById("overlay");
+document.addEventListener("DOMContentLoaded", () => {
+  const openModalButton = document.getElementById("open-modal-button");
+  const closeModalButtons = document.querySelectorAll("[data-close-button]");
+  const overlay = document.getElementById("overlay");
 
-openModalButton.addEventListener("click", () => {
-  openModal(modal);
-});
+  openModalButton.addEventListener("click", () => {
+    openModal(modal);
+  });
 
-overlay.addEventListener("click", () => {
-  const modal = document.querySelector(".addModal.active");
-  closeModal(modal);
-});
-
-closeModalButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const modal = button.closest(".addModal");
+  overlay.addEventListener("click", () => {
+    const modal = document.querySelector(".addModal.active");
     closeModal(modal);
   });
-});
 
-function openModal(modal) {
-  if (modal == null) return;
-  modal.classList.add("active");
-  overlay.classList.add("active");
-}
-function closeModal(modal) {
-  if (modal == null) return;
-  modal.classList.remove("active");
-  overlay.classList.remove("active");
-}
+  closeModalButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const modal = button.closest(".addModal");
+      closeModal(modal);
+    });
+  });
 
-const myForm = document.getElementById("myform");
+  function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add("active");
+    overlay.classList.add("active");
+  }
+  function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  }
 
-const _handleSubmit = async (e) => {
-  e.preventDefault();
-  const proposition = document.getElementById("proposition").value;
-  const description = document.getElementById("description").value;
-  console.log("submited");
+  const myForm = document.getElementById("myform");
 
-  const data = {
-    proposition,
-    description,
+  const _handleSubmit = async (e) => {
+    e.preventDefault();
+    const proposition = document.getElementById("proposition").value;
+    const description = document.getElementById("description").value;
+    console.log("submited");
+
+    const data = {
+      proposition,
+      description,
+    };
+    addproposition(data);
+    myForm.reset();
   };
-  addproposition(data);
-  myForm.reset();
-};
 
-function addproposition(data) {
-  const newProposition = document.createElement("li");
-  newProposition.classList.add("list-group-item");
-  newProposition.classList.add("p-1");
-  newProposition.innerHTML = `<div class="d-flex p-1 flex-column flex-md-row">
+  function addproposition(data) {
+    const newProposition = document.createElement("li");
+    newProposition.classList.add("list-group-item");
+    newProposition.classList.add("p-1");
+    newProposition.innerHTML = `<div class="d-flex p-1 flex-column flex-md-row">
     <div class="d-flex flex-column">
     <a class="pseudo" href="#">Pseudo :</a>
     <small class="text-muted">date</small>
@@ -59,22 +60,14 @@ function addproposition(data) {
     <button type="button" class="btn btn-sm btn-primary description-popover" data-toggle="popover" data-placement="top" 
     title="description" data-content="${data.description}">Show description</button>
     </div></div></li>`;
-  document.getElementById("propositionListe").appendChild(newProposition);
+    document.getElementById("propositionListe").appendChild(newProposition);
 
-  $(function () {
-    $(".description-popover").popover({
-      container: "body",
+    $(function () {
+      $(".description-popover").popover({
+        container: "body",
+      });
     });
-  });
-}
+  }
 
-myForm.addEventListener("submit", _handleSubmit);
-
-$(document).ready(function () {
-  $("#searchInput").on("keyup", function () {
-    var value = $(this).val().toLowerCase();
-    $("#propositionListe *").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-    });
-  });
+  myForm.addEventListener("submit", _handleSubmit);
 });
