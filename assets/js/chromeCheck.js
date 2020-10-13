ccd0afe4d94e75d09a8cf19513111ee16eff1a98
Yaco-99 <coopman.yannick@gmail.com>
2020-10-13 13:13:13 +0200

    minified css & font swap

---
+++ assets/js/chromeCheck.js
@@ -0,0 +1,7 @@
+const is_chrome =
+  navigator.userAgent.toLocaleLowerCase().indexOf("chrome") > -1;
+const link = document.createElement("link");
+link.href = "assets/css/chromeMin.css";
+link.rel = "stylesheet";
+link.type = "text/css";
+is_chrome ? document.head.appendChild(link) : "not chrome";
