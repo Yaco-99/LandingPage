6c3879b914b5ea311a26c06f63bc304441f2a521
Yaco-99 <coopman.yannick@gmail.com>
2020-10-13 13:51:02 +0200

    litle optimisation

--- assets/js/chromeCheck.js
+++ assets/js/chromeCheck.js
@@ -2,6 +2,6 @@ const is_chrome =
   navigator.userAgent.toLocaleLowerCase().indexOf("chrome") > -1;
 const link = document.createElement("link");
 link.href = "assets/css/chromeMin.css";
-link.rel = "stylesheet";
+link.rel = "preload";
 link.type = "text/css";
 is_chrome ? document.head.appendChild(link) : "not chrome";
